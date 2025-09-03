e54cde65facce26a5f9ce9c115055005dcf17fd2
Prensu <dangolprensu17newar@gmail.com>
2025-09-03 15:33:54 +0545

    Updata  main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import ShopContextProvider from './Context/ShopContext.jsx'
+import {ShopContextProvider } from './Context/ShopContext.jsx'
 
 
 //now for the whole project we will get the help of react router Dom
